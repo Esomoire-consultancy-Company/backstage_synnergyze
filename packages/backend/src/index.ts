ab40585673cd167644662bf65e9065a64d2d8996
Faiz Ahmed <193629930+Esomoire-consultancy-Company@users.noreply.github.com>
2026-09-22 20:39:42 +0530

    feat(permission): replace allow-all with Synnergyze context policy

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -53,10 +53,8 @@ backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(import('@backstage/plugin-events-backend'));
 backend.add(import('@backstage/plugin-devtools-backend'));
 backend.add(import('@backstage/plugin-kubernetes-backend'));
-backend.add(
-  import('@backstage/plugin-permission-backend-module-allow-all-policy'),
-);
 backend.add(import('@backstage/plugin-permission-backend'));
+backend.add(import('./synnergyzePermissionPolicy'));
 backend.add(import('@backstage/plugin-proxy-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
